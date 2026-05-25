33ea16f32928506f410bbfc9c05ad033ea4fd2b7
Kristoffer-L <kristoffer.larsson.02@gmail.com>
2026-05-25 18:22:19 +0200

    ft:Integration of chess rules and design

--- src/App.tsx
+++ src/App.tsx
@@ -1,122 +1,91 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+import { Chess } from "chess.js";
+import { Chessboard } from "react-chessboard";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [game, setGame] = useState(new Chess());
+  const [status, setStatus] = useState<string>("playing");
+
+  function updateStatus(gameCopy: Chess) {
+    if (gameCopy.isCheckmate()) {
+      const loser = gameCopy.turn() === "w" ? "White" : "Black";
+      const winner = loser === "White" ? "Black" : "White";
+      setStatus(`Checkmate! You ${winner === "White" ? "win" : "lost"}`);
+    } else if (gameCopy.isStalemate()) {
+      setStatus("stalemate");
+    } else if (gameCopy.isDraw()) {
+      setStatus("Draw 🤝");
+    } else {
+      setStatus("playing");
+    }
+  }
+
+  function onPieceDrop({
+    sourceSquare,
+    targetSquare,
+  }: {
+    sourceSquare: string | null;
+    targetSquare: string | null;
+  }): boolean {
+    if (!sourceSquare || !targetSquare) return false;
+
+    const gameCopy = new Chess(game.fen());
+
+    const move = gameCopy.move({
+      from: sourceSquare,
+      to: targetSquare,
+      promotion: "q",
+    });
+
+    if (!move) return false;
+
+    setGame(gameCopy);
+    updateStatus(gameCopy);
+
+    return true;
+  }
+
+  function resetGame() {
+    setGame(new Chess());
+    setStatus("playing");
+  }
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+      <section className="header"></section>
+      <section className="main">
+        <div className="profile-container">
+          <div
+            className="player-user-card"
+            style={{ borderColor: game.turn() === "w" ? "yellow" : "gray" }}
+          >
+            <p>User:</p>
+            <div>Name 1</div>
+          </div>
+          <div
+            className="player-opponent-card"
+            style={{ borderColor: game.turn() === "w" ? "gray" : "yellow" }}
+          >
+            <p>Opponent:</p>
+            <div>Name 2</div>
+          </div>
         </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
+        <div className="status-container">
+          <h1>{status}</h1>
         </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
+        <div className="chess-board">
+          <Chessboard
+            options={{
+              position: game.fen(),
+              onPieceDrop,
+            }}
+          />
+        </div>
+        <button onClick={resetGame}>Reset</button>
       </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
