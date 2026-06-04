f7d52d4043372c18b97b9ab6b7a70a1d59c76f33
Kristoffer-L <kristoffer.larsson.02@gmail.com>
2026-06-04 16:22:14 +0200

    test: trying the api_url for render

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
-const API_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:3000";
-
+// const API_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:3000";
+const API_URL = "https://your-backend.onrender.com";
 export { API_URL };
