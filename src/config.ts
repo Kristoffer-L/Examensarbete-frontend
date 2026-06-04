3a5731bf5ff6369e3143ed816ceacc6c0e6a500f
Kristoffer-L <kristoffer.larsson.02@gmail.com>
2026-06-04 16:30:08 +0200

    test: trying the api_url for render

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 // const API_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:3000";
-const API_URL = "https://your-backend.onrender.com";
+const API_URL = "https://https://examensarbete-backend-2469.onrender.com/";
 export { API_URL };
